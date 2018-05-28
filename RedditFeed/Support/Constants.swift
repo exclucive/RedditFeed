d666f65c87762621d900f72512d98ef1460edea5
Igor Novik <exclucive@yandex.ru>
2018-05-28 16:17:55 -0700

    created project structure

---
+++ RedditFeed/Support/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RedditFeed
+//
+//  Created by Igor Novik on 2018-05-28.
+//  Copyright © 2018 NAppsLab. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+
+}
