73b8d9e73222d43212512d4ea2e05e6fc1fea469
Igor Novik <exclucive@yandex.ru>
2018-05-28 16:10:22 -0700

    first commit

---
+++ RedditFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditFeed
+//
+//  Created by Igor Novik on 2018-05-28.
+//  Copyright © 2018 NAppsLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
