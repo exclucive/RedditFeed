d0fc0ae7ab14977e2a2f2fea4eee262210000b88
Igor Novik <exclucive@yandex.ru>
2018-05-30 23:36:38 -0700

    added saving logic

--- RedditFeed/Controllers/FullEntryImageViewController.swift
+++ RedditFeed/Controllers/FullEntryImageViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class FullEntryImageViewController: UIViewController {
-    var imageURL: URL?
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var imageURL: URL?
     private let imageDownloader = RedditImageDownloader()
     
     override func viewDidLoad() {
@@ -26,13 +28,36 @@ class FullEntryImageViewController: UIViewController {
     
     // MARK: Private
     func setupImage() {
+        activityIndicator.startAnimating()
         imageDownloader.setImage(toImageView: imageView, withURL: imageURL, placeholder: UIImage(named: "placeholder")) {[unowned self] (success) in
             self.saveButton.isEnabled = true
+            self.activityIndicator.stopAnimating()
         }
     }
     
     // MARK: Actions
     @IBAction func saveButtonAction(_ sender: Any) {
+        if let image = imageView.image {
+            activityIndicator.startAnimating()
+            saveButton.isEnabled = false
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        activityIndicator.stopAnimating()
+        saveButton.isEnabled = true
         
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
 }
